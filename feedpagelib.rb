2ddac1f12ac78e64a14df8803640c513388097fb
Elias Mulhall <eli.mulhall@gmail.com>
2016-06-29 02:39:28 -0400

    change how log works again, do not exit if feed fails to fetch

--- feedpagelib.rb
+++ feedpagelib.rb
@@ -4,24 +4,26 @@ require 'erb'
 require 'yaml'
 
 # fetch rss content, build page, and handle errors
-def new_feedpage url_list, template, html_out, required_fields, feeds_proc, log: nil
+def new_feedpage url_list, template, html_out, required_fields, feeds_proc, 
+                 log: nil, print_progress: false
   begin
     check_file_exists(template)
     check_file_exists(url_list)
     urls = YAML.load_file(url_list)
-    rss_content = process_feeds(urls, required_fields, feeds_proc, log)
-    build_and_save_page(rss_content, template, html_out)
+    rss_content = process_feeds(urls, required_fields, feeds_proc, 
+                                log, print_progress)
+    build_and_save_page(rss_content, template, html_out, print_progress)
   rescue Exception => e
-    debug(log, e.message)
-    raise e
+    write_log(log, e.message) if log
+    raise e if print_progress 
+    exit(1)
   end
 end
 
 private
 
-def debug log, message
-  puts message
-  open(log, 'a') { |f| f.puts "#{Time.now}\t#{message}" } if log
+def write_log log, message
+  open(log, 'a') { |f| f.puts "#{Time.now}\t#{message}" }
 end
 
 # throw exception if file doesn't exist
@@ -32,14 +34,26 @@ end
 # Get needed rss content from feed urls. Fetch the content with feedjira,
 # filter out all feeds missing information, and apply the user defined
 # feed content proc to collect the needed fields.
-def process_feeds urls, required_fields, feeds_proc, log
-  feeds = urls.map { |url| [url, Feedjira::Feed.fetch_and_parse(url)] }
+def process_feeds urls, required_fields, feeds_proc, log, print_progress
+  feeds = urls.map do |url| 
+    begin
+      puts "Fetching #{url}" if print_progress
+      [url, Feedjira::Feed.fetch_and_parse(url)] 
+    rescue Feedjira::FetchFailure
+      write_log(log, "Failed to fetch #{url}") if log
+      puts "Failed to fetch #{url}" if print_progress
+      nil
+    end
+  end.compact
   complete_feeds = feeds.select do |url, feed|
     required_fields.map do |field_name| 
       call_res = field_name.split('.').inject(feed) do |acc, subfield|
         acc.respond_to?(subfield) ? acc.send(subfield.to_sym) : nil
       end
-      debug(log, "#{url} missing field '#{field_name}'") if call_res.nil?
+      if call_res.nil?
+        write_log(log, "#{url} missing field '#{field_name}'") if log
+        puts "#{url} missing field '#{field_name}', skip" if print_progress
+      end
       call_res
     end
     .none? { |field| field.nil? }
@@ -49,7 +63,8 @@ def process_feeds urls, required_fields, feeds_proc, log
 end
 
 # build html from template, save to file
-def build_and_save_page rss_content, template, html_out
+def build_and_save_page rss_content, template, html_out, print_progress
+  puts "Generating html file" if print_progress
   # save feed content to a variable accessable from erb file
   @rss_content = rss_content
   # intermediary file names
